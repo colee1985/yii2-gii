afde2108a134ca5ac4d7364ac7c202c5344ab771
colee <5969226@qq.com>
2016-04-01 17:04:17 +0800

    add test file

---
+++ fixtures/default/test.php
@@ -0,0 +1,69 @@
+<?php
+
+
+echo "<?php\n";
+?>
+namespace tests\codeception\common\unit\models;
+
+use Yii;
+use tests\codeception\common\unit\DbTestCase;
+use Codeception\Specify;
+use boss\models\system\LoginForm;
+use tests\codeception\common\fixtures\UserFixture;
+
+/**
+ * Login form test
+ */
+class LoginFormTest extends DbTestCase
+{
+
+    use Specify;
+    /** DEMO
+    public function setUp()
+    {
+        parent::setUp();
+
+        Yii::configure(Yii::$app, [
+            'components' => [
+                'user' => [
+                    'class' => 'yii\web\User',
+                    'identityClass' => 'dbbase\models\system\SystemUser',
+                ],
+            ],
+        ]);
+    }
+
+    protected function tearDown()
+    {
+        Yii::$app->user->logout();
+        parent::tearDown();
+    }
+
+    public function testLoginNoUser()
+    {
+        $model = new LoginForm([
+            'username' => 'not_existing_username',
+            'password' => 'not_existing_password',
+        ]);
+
+        $this->specify('user should not be able to login, when there is no identity', function () use ($model) {
+            expect('model should not login user', $model->login())->false();
+            expect('user should not be logged in', Yii::$app->user->isGuest)->true();
+        });
+    }
+    */
+
+    /**
+     * @inheritdoc
+     */
+    public function fixtures()
+    {
+        return [
+            '<?php echo $model->formName()?>' => [
+                'class' => UserFixture::className(),
+                'dataFile' => '@tests/codeception/common/fixtures/data/<?=$schema->name?>.php'
+            ],
+        ];
+    }
+
+}
